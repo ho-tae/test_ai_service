b57d5ef2b0f0cea0c216564ca5189e49e48d7564
astromingha <innopam.mh@gmail.com>
2021-06-17 19:06:53 +0900

    dependency setup

---
+++ ai-service/test/ENV_install.sh
@@ -0,0 +1,11 @@
+pip install wheel
+pip install numpy==1.19.4
+pip install torch==1.6.0
+pip install torchvision==0.7.0
+pip install scikit-build
+pip install mmcv-full==1.2.0
+cd inference_module
+pip install -r requirements/build.txt
+pip install -v -e .  # or "python setup.py develop"
+cd ..
+export PYTHONPATH=$`pwd`/inference_module
